736f551d2f871192e45c0a1b8139ed2a4f360879
amalivert <amalivert@gmail.com>
2021-08-21 18:03:16 -0400

    will remove later

--- docs/docsite/source/conf.py
+++ docs/docsite/source/conf.py
@@ -30,8 +30,15 @@ release = '2.4.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+#extensions = [
+#]
+
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+	]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
